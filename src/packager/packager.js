05546c6e7e3c378c26e2b0ff6a58ae2c9df33402
Thomas Weber <weber_t@mailbox.org>
2021-07-06 17:25:52 -0500

    electron: Hide window until ready to show

--- src/packager/packager.js
+++ src/packager/packager.js
@@ -429,7 +429,9 @@ const createWindow = () => {
       contextIsolation: true,
       nodeIntegration: false,
       sandbox: true
-    }
+    },
+    show: false,
+    backgroundColor: ${JSON.stringify(this.options.appearance.background)}
   };
 
   const activeScreen = screen.getDisplayNearestPoint(screen.getCursorScreenPoint());
@@ -438,6 +440,9 @@ const createWindow = () => {
   options.y = bounds.y + ((bounds.height - options.height) / 2);
 
   const window = new BrowserWindow(options);
+  window.once('ready-to-show', () => {
+    window.show();
+  });
   window.loadFile('../../index.html');
 };
 
